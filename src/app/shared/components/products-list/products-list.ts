91bb2b03c7e2fc963186dfd84bb76aa03f6a3cb4
FVS-MBA <feliven@gmail.com>
2026-07-07 20:59:10 -0300

    obtém dados do Supabase e exibe na lista de produtos

--- src/app/shared/components/products-list/products-list.ts
+++ src/app/shared/components/products-list/products-list.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal, type OnInit } from '@angular/core';
 import type { Product } from '../../types/types';
 import { ProductCard } from '../product-card/product-card';
+import { SupabaseService } from '../../services/supabase.service';
 
 @Component({
   selector: 'app-products-list',
@@ -8,8 +9,29 @@ import { ProductCard } from '../product-card/product-card';
   templateUrl: './products-list.html',
   styleUrl: './products-list.css',
 })
-export class ProductsList {
+export class ProductsList implements OnInit {
+  private supabaseService = inject(SupabaseService);
+
+  products = signal<Product[]>([]);
   productsByCategory = signal<{ category: string; products: Product[] }[]>([]);
+
+  ngOnInit(): void {
+    this.loadProducts().then(() => {
+      this.groupProductsByCategory(this.products());
+      console.table(this.productsByCategory());
+    });
+  }
+
+  async loadProducts() {
+    try {
+      const data = await this.supabaseService.getProducts();
+      this.products.set(data);
+
+      console.table(this.products());
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  }
 
   groupProductsByCategory(products: Product[]) {
     const categories = [
