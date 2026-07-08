3d3739e41273f08b5f557b676048cf8b9d60f042
FVS-MBA <feliven@gmail.com>
2026-07-07 21:52:55 -0300

    obtém localização do naegador para exemplificar erro no SSR

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, type OnInit } from '@angular/core';
 import { ProductsList } from '../../shared/components/products-list/products-list';
 
 @Component({
@@ -7,4 +7,14 @@ import { ProductsList } from '../../shared/components/products-list/products-lis
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  ngOnInit(): void {
+    try {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(JSON.stringify(position.coords.toJSON(), null, ' '));
+      });
+    } catch (error) {
+      console.warn('Acesso à localização não foi liberado', error);
+    }
+  }
+}
