f6c43a06cefa244f6e17d04a1e20144eefef725f
FVS-MBA <feliven@gmail.com>
2026-07-07 21:12:29 -0300

    exibe dados do backend na página de cada produto

--- src/app/shared/components/product-detail/product-detail.ts
+++ src/app/shared/components/product-detail/product-detail.ts
@@ -1,6 +1,5 @@
-import { AsyncPipe, CurrencyPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject, signal, type OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import type { Product } from '../../types/types';
 import { MatCardModule } from '@angular/material/card';
@@ -8,29 +7,37 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { SupabaseService } from '../../services/supabase.service';
 
 @Component({
   selector: 'app-product-detail',
   imports: [
     RouterLink,
-    AsyncPipe,
     CurrencyPipe,
     MatCardModule,
     MatIconModule,
+    MatButtonModule,
     MatFormFieldModule,
     MatSelectModule,
     MatCheckboxModule,
+    MatInputModule,
   ],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
 })
-export class ProductDetail {
+export class ProductDetail implements OnInit {
   quantities: number[] = [1, 2, 3, 4, 5];
-  product$!: Observable<Product | null>;
+  product = signal<Product | null>(null);
 
   private route = inject(ActivatedRoute);
+  private supabaseService = inject(SupabaseService);
 
   ngOnInit(): void {
     const productId = parseInt(this.route.snapshot.paramMap.get('id') ?? '0', 10);
+    this.supabaseService.getProductById(productId).then((data) => {
+      this.product.set(data || null);
+    });
   }
 }
