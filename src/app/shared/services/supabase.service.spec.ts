6f68fb55b19b232a0846698b0708827a7d30ee1f
FVS-Thiago <feliven@gmail.com>
2026-07-13 20:36:47 -0300

    melhora teste do Supabase

--- src/app/shared/services/supabase.service.spec.ts
+++ src/app/shared/services/supabase.service.spec.ts
@@ -2,11 +2,18 @@ import { TestBed } from '@angular/core/testing';
 
 import { SupabaseService } from './supabase.service';
 
+const supabaseServiceMock: Partial<SupabaseService> = {
+  getProducts: vi.fn().mockResolvedValue([]),
+  getProductById: vi.fn().mockResolvedValue(null),
+};
+
 describe('SupabaseService', () => {
   let service: SupabaseService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: SupabaseService, useValue: supabaseServiceMock }],
+    });
     service = TestBed.inject(SupabaseService);
   });
 
