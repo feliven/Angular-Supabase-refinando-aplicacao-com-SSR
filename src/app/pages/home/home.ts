79ef4081710ee4eedaf666c54e3d53074c7c2e5e
FVS-MBA <feliven@gmail.com>
2026-07-17 20:32:33 -0300

    implementa título dinâmico e metadados na home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { afterNextRender, Component, type OnInit } from '@angular/core';
+import { afterNextRender, Component, inject, type OnInit } from '@angular/core';
 import { ProductsList } from '../../shared/components/products-list/products-list';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,10 @@ import { ProductsList } from '../../shared/components/products-list/products-lis
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
+  private readonly title = inject(Title);
+  private readonly meta = inject(Meta);
+
   constructor() {
     afterNextRender(() => {
       if (navigator.geolocation) {
@@ -18,5 +22,32 @@ export class Home {
         console.warn('Acesso à localização não foi liberado');
       }
     });
+  }
+
+  ngOnInit(): void {
+    this.setTitleMetadata();
+  }
+
+  setTitleMetadata() {
+    this.title.setTitle('Deleite - dê leite!');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: 'Descubra os melhores milkshakes, sorvetes e smoothies na Deleite',
+      },
+      { property: 'og:title', content: 'Deleite - dê leite!' },
+      {
+        property: 'og:description',
+        content: 'Descubra os melhores milkshakes, sorvetes e smoothies na Deleite',
+      },
+      {
+        property: 'og:image',
+        content: 'assets/images/logo.png',
+      },
+      {
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+    ]);
   }
 }
