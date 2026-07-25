590230306079265d36a1cf9fae7de5171b16e0c7
FVS-MBA <feliven@gmail.com>
2026-07-25 14:28:08 -0300

    prepara projeto para deploy

--- src/app/shared/services/supabase.client.ts
+++ src/app/shared/services/supabase.client.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
-export const supabaseClient = createClient(environment.supabaseUrl, environment.supabaseKey);
+export const supabaseClient = createClient(environment.supabaseUrl, environment.supabaseAnonKey);
